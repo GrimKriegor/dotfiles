f84387030121c76da79914aa5622678b27c745c7
Grim Kriegor <grimkriegor@krutt.org>
2018-03-02 01:00:24 +0000

    [Polybar] Multi monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,10 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar example &
+for i in $(polybar -m | awk -F: '{print $1}'); do
+  echo -e "Monitor: $i"
+  export MONITOR="$i"
+  polybar top -c ~/.config/polybar/config &
+done
 
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top -c ~/.config/polybar/config & done
 feh --bg-scale ~/.config/wall.jpg
 
 echo "Bars launched..."
